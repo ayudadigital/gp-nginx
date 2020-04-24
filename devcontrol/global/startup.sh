e19c4a2f526cb560a69c20c4385b61962ae02342
Pedro Amador <pedroamador.rodriguez@gmail.com>
2020-04-24 15:59:12 +0200

    Upgrade: Change tpbtools and TIC para Bien references with Ayuda Digital

--- devcontrol/global/startup.sh
+++ devcontrol/global/startup.sh
@@ -2,5 +2,5 @@
 
 # @file devcontrol/global/startup.sh
 # @brief devcontrol startup script and functions
-echo "Generic Platform - Nginx Statics Server Product (c) 2020 TIC para Bien"
+echo "Generic Platform - Nginx Statics Server Product (c) 2020 Ayuda Digital"
 echo
